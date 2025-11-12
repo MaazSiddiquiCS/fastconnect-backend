0838722edf141c3c8fb98383e71808e5a0f9128d
MaazSiddiqiCS <muhammadmaazsiddiqi@gmail.com>
2025-11-12 21:15:22 +0500

    Start of project

---
+++ src/main/java/com/fastconnect/FastConnectApplication.java
@@ -0,0 +1,13 @@
+package com.fastconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastconnect/FastConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
