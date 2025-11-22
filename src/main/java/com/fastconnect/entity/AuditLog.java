0b41c2bb18ad86efc5b4138ea24ab56398943769
MaazSiddiqiCS <muhammadmaazsiddiqi@gmail.com>
2025-11-23 00:30:21 +0500

    AuditLog entity created -Enums created
    ActionType, EntityType, FlagReason, FlagStatus, ModerationStatus

---
+++ src/main/java/com/fastconnect/entity/AuditLog.java
@@ -0,0 +1,54 @@
+package com.fastconnect.entity;
+
+import com.fastconnect.enums.ActionType;
+import com.fastconnect.enums.EntityType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "audit_logs")
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "logs_seq")
+    @SequenceGenerator(
+            name = "logs_seq",
+            sequenceName = "logs_sequence",
+            allocationSize = 50
+    )
+    private Long log_id;
+
+    @NotNull(message = "Required action type")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 50)
+    private ActionType action_type;
+
+    @NotNull(message = "Required enity type")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 50)
+    private EntityType entity_type;
+
+    @NotNull(message = "Required The ID of the specific entity affected.")
+    @Column(nullable = false)
+    private Long entity_id;
+
+    @NotNull
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime action_performed_at;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "action_user_id")
+    private User actionUser;
+
+    @PrePersist
+    public void prePersist() {
+        if (action_performed_at == null) {
+            action_performed_at = LocalDateTime.now();
+        }
+    }
+}
---
+++ src/main/java/com/fastconnect/enums/FlagReason.java
@@ -0,0 +1,11 @@
+package com.fastconnect.enums;
+
+public enum FlagReason {
+    SPAM,
+    HATE_SPEECH,
+    HARASSMENT,
+    NUDITY,
+    OFF_TOPIC,
+    FALSE_INFORMATION,
+    SELF_HARM
+}
